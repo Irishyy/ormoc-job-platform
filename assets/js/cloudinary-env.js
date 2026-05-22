be7ce848f85b403b603eab5adcce30a06bb8034d
Irishyy <erishemzzz@gmail.com>
2026-05-23 07:25:23 +0800

    remove Cloudinary environment configuration file

--- assets/js/cloudinary-env.js
+++
@@ -1,8 +0,0 @@
-// Cloudinary settings for unsigned browser uploads (upload preset only).
-// Do NOT put api_secret here. Keep secrets in config/DatabaseConnection.php for PHP curl.
-
-const CloudinaryEnv = {
-  cloudName: "dxrvbwycv",
-  uploadPreset: "ormoc_job_matching_preset",
-  uploadUrl: "https://api.cloudinary.com/v1_1/dxrvbwycv/image/upload"
-};
